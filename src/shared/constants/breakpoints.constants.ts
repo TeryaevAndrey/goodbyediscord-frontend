9d589f5ee06ac2f2a4d0db998bc56ef0d2a8e833
Andrey <teryaev.2004@mail.ru>
2024-10-19 18:29:18 +0300

    Added breakpoints to constants

---
+++ src/shared/constants/breakpoints.constants.ts
@@ -0,0 +1,5 @@
+export const mobile = window.matchMedia("(max-width: 600px)");
+export const tablet = window.matchMedia(
+  "(min-width: 601px) and (max-width: 1023px)"
+);
+export const desktop = window.matchMedia("(min-width: 1024px)");
