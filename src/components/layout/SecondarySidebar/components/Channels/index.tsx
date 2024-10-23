47b24bfe61bc37e223643aa627e750b96845ccc9
TeryaevAndrey <teryaev.2004@mail.ru>
2024-10-23 22:38:35 +0300

    channels / Channels list to only component in secondary sidebar

--- src/components/layout/SecondarySidebar/components/Channels/index.tsx
+++ src/components/layout/SecondarySidebar/components/Channels/index.tsx
@@ -1,11 +1,51 @@
-import { MainComponentProps } from "@/shared/types"
-import { cn } from "@/shared/utils"
-import { FC } from "react"
+import { Channel } from "@/components/entities";
+import { Button } from "@/components/ui";
+import { MainComponentProps } from "@/shared/types";
+import { cn } from "@/shared/utils";
+import { FC } from "react";
+import { IoIosAdd } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
-export const Channels: FC<MainComponentProps> = ({children, className}) => {
-  return(
-    <div className={cn('flex flex-col', className)}>
-      {children}
+export const Channels: FC<MainComponentProps> = ({ className }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={cn("flex flex-col", className)}>
+      <Button
+        variant="transparent"
+        mode="square"
+        onClick={() => navigate("/channel/123")}
+      >
+        <Channel />
+      </Button>
+      <Button variant="transparent" mode="square">
+        <Channel />
+      </Button>
+      <Button variant="transparent" mode="square">
+        <Channel />
+      </Button>
+      <Button variant="transparent" mode="square">
+        <Channel />
+      </Button>
+      <Button variant="transparent" mode="square">
+        <Channel />
+      </Button>
+      <Button variant="transparent" mode="square">
+        <Channel />
+      </Button>
+      <Button variant="transparent" mode="square">
+        <Channel />
+      </Button>
+
+      <Button
+        variant="transparent"
+        mode="square"
+        onClick={() =>
+          document.getElementById("add_channel_modal")?.showModal()
+        }
+      >
+        <IoIosAdd className="fill-base-content" size={28} />
+      </Button>
     </div>
-  )
-}
+  );
+};
--- src/components/layout/SecondarySidebar/index.tsx
+++ src/components/layout/SecondarySidebar/index.tsx
@@ -2,50 +2,13 @@ import { FiSettings } from "react-icons/fi";
 import { Button } from "@/components/ui";
 import { CurrentUser } from "@/components/shared";
 import { Channels } from "./components";
-import { Channel } from "@/components/entities";
-import { IoIosAdd } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
 
 export const SecondarySidebar = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="w-[90px] bg-base-200 h-[100dvh] py-6 flex flex-col justify-between items-center gap-4">
       <CurrentUser />
 
-      <Channels className="mb-auto mt-4">
-        <Button variant="transparent" mode="square" onClick={() => navigate("/channel/123")}>
-          <Channel />
-        </Button>
-        <Button variant="transparent" mode="square">
-          <Channel />
-        </Button>
-        <Button variant="transparent" mode="square">
-          <Channel />
-        </Button>
-        <Button variant="transparent" mode="square">
-          <Channel />
-        </Button>
-        <Button variant="transparent" mode="square">
-          <Channel />
-        </Button>
-        <Button variant="transparent" mode="square">
-          <Channel />
-        </Button>
-        <Button variant="transparent" mode="square">
-          <Channel />
-        </Button>
-
-        <Button
-          variant="transparent"
-          mode="square"
-          onClick={() =>
-            document.getElementById("add_channel_modal")?.showModal()
-          }
-        >
-          <IoIosAdd className="fill-base-content" size={28} />
-        </Button>
-      </Channels>
+      <Channels className="mb-auto mt-4" />
 
       <div className="flex flex-col gap-4">
         <Button variant="secondary" mode="square">
