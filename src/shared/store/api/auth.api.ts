9cd99ff2b5074c5dcbcffdc07982dd9e28dccc66
TeryaevAndrey <teryaev.2004@mail.ru>
2024-10-26 01:23:36 +0300

    channels / rework sign in, sign up

--- src/shared/store/api/auth.api.ts
+++ src/shared/store/api/auth.api.ts
@@ -10,7 +10,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API!}/auth`,
+    baseUrl: `${import.meta.env.VITE_API!}/user_auth`,
   }),
 
   endpoints: ({ query, mutation }) => ({
